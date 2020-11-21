f286ff082291420f82412c27b3188da2efacc188
dipin <dipinraj.d@gmail.com>
2020-11-22 00:13:01 +0530

    //showDrivers() commented

--- js/googlemap.js
+++ js/googlemap.js
@@ -167,7 +167,7 @@ function initMap() {
       //the watchposition() method show the position of the user and update it while is moving
         navigator.geolocation.watchPosition(showPosition);
     } 
-    showDrivers();
+    //showDrivers();
 }
 
 function showDrivers(lat, lon) {
